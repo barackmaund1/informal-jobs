98ff603438fca2ebdbba839aa213ef2908fb46e3
barackmaund1 <pmaundu49@gmail.co>
2020-03-04 09:19:19 +0300

    changed files

---
+++ js/abroad.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+   
+
+    $("#sign").click(function(event) {
+     $("#job").hide();
+     $(".create1").slideDown('1000');
+    })
+   $("#job").click(function() {
+        $("#sign").hide("1500");
+        $(".create2").slideDown('1000');
+        
+    })
+    $(".cancelbtn").click(function(event) {
+        $(".create2").hide("1500");
+        $("#job").slideDown("1000");
+        $("#sign").slideDown("1000")
+    })
+
+    $("#cancelbtn").click(function(event) {
+       $(".create1").hide("1500");
+       $("#job").slideDown("1000");
+       $("#sign").slideDown("1000")
+   })
+    event.preventDefault()
+})
