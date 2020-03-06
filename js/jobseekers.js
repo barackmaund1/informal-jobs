e33b1a4feac35d2fe7d6e5008a0f23476c998f2a
monte <eogenche@yahoo.com>
2020-03-06 12:39:35 +0300

    merge

--- js/jobseekers.js
+++ js/jobseekers.js
@@ -1,4 +1,4 @@
- function salidate(){
+function salidate(){
 const FirstName = document.getElementById("FirstName").value;
 const SecondName = document.getElementById("SecondName").value;
 const Age = document.getElementById("Age").value;
@@ -6,34 +6,22 @@ const Phone = document.getElementById("PhoneNumber").value;
 const EducationLevel = document.getElementById("EducationLevel").value;
 
 if(FirstName !== "" && SecondName !== "" && Age!=="" && Phone!=="" && EducationLevel!==""){
-    alert("All details filled");
-    
+    alert("I was satisfied");
+    // releaseEvents();
   }
   else {
   alert("Form is incomplete");
-  someName();
+  
   }
-}
-
-// function releaseEvents(){
-
-  $(document).ready(function(){
+$(document).ready(function(){
 $("#submit3").click(function(event){
     event.preventDefault()
     $(".form").toggle(1000)
     $(".form2").toggle(1000)
   }); 
-
-  $("#submit1").click(function(event){
-    event.preventDefault()
-    $(".form2").hide(2000);
-    $(".form3").show(2000); 
-  });
  });
-
-
+}
 function someName(){
-
 var total=0;
 var answer1 = document.getElementById("answer1").value;
 var answer = document.getElementById("answer").value;
@@ -42,9 +30,13 @@ var answer2 = document.getElementById("answer2").value;
   if(answer === "" || answer1 === "" || answer2 ===""){
     alert("Please answer all questions");
     
-    }
-    else{
-      alert("We moving on");
-
-    }
   }
+  else{
+    alert("We moving on");
+``}
+$("#submit1").click(function(event){
+  event.preventDefault()
+  $(".form2").hide(2000);
+  $(".form3").show(2000); 
+});
+}
